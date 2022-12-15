15349f2ee2cb4a5edc2b9b72c40192d209bb1758
JasminSalsabilaArifah <sabilarifa29@gmail.com>
2022-12-15 19:31:04 +0700

    new

---
+++ statefull_widget_03/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:statefull_widget/widget/Input.dart';
+import 'package:statefull_widget/widget/Result.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  TextEditingController _inputUser = new TextEditingController();
+  double _inputCelcius = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+  void convert() {
+    setState(() {
+      _inputCelcius = double.parse(_inputUser.text);
+      _reamur = 4 / 5 * _inputCelcius;
+      _kelvin = _inputCelcius + 273;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text("Konvert Suhu"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Input(inputUser: _inputUser),
+              Result(kelvin: _kelvin, reamur: _reamur),
+              Container(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                    onPressed: convert, child: Text("Konversi Suhu")),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
